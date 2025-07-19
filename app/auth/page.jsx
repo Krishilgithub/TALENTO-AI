c724c927c6f134017f7172b7294861c006018a09
Dhyan_Mehta <dhyanm2701@gmail.com>
2025-07-19 22:43:07 +0530

    Resolve Deployment Issues

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,11 +1,10 @@
-import AuthForm from '@/components/Forms/AuthForm'
 import React from 'react'
 
 const Page = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen gap-4'>
       <h1 className='text-4xl font-bold'>Not Authenticated</h1>
-      <AuthForm />
+      <p className='text-gray-400'>You do not have access to this page.</p>
     </div>
   )
 }
